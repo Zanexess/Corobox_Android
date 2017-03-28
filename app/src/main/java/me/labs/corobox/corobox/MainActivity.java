68a0be2040b765e0772ebb7331dd8b99a8be60c9
Zanexess <sesldark@yandex.ru>
2017-03-28 23:49:45 +0300

    Crashlytics added

--- app/src/main/java/me/labs/corobox/corobox/MainActivity.java
+++ app/src/main/java/me/labs/corobox/corobox/MainActivity.java
@@ -3,12 +3,15 @@ package me.labs.corobox.corobox;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
+import com.crashlytics.android.Crashlytics;
+import io.fabric.sdk.android.Fabric;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Fabric.with(this, new Crashlytics());
         setContentView(R.layout.activity_main);
 
         TextView textView = (TextView) findViewById(R.id.textView);
