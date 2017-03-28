45c87f6080d9315830de8f1984bd22cfe184aff0
Zanexess <sesldark@yandex.ru>
2017-03-28 23:46:24 +0300

    Fastlane added

--- app/src/main/java/me/labs/corobox/corobox/MainActivity.java
+++ app/src/main/java/me/labs/corobox/corobox/MainActivity.java
@@ -2,6 +2,7 @@ package me.labs.corobox.corobox;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView textView = (TextView) findViewById(R.id.textView);
+        textView.setText(BuildConfig.VERSION_NAME);
+
     }
 }
